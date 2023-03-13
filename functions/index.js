09f9c96fe43caeb926d7e6cdcecc949beaff60bb
json <json@kakao.com>
2023-03-13 17:15:03 +0900

    인증된 토큰 유저만 호출할 수 있게 로직 수정

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,36 @@
 const functions = require('firebase-functions')
 const { BigQuery } = require('@google-cloud/bigquery')
+const { OAuth2Client } = require('google-auth-library')
 const useCrawling = require('./crawling')
+const client = new OAuth2Client(process.env.CLIENT_ID)
 
 exports.crwalOneplusDeal = functions
   .region('asia-northeast3')
   .runWith({ timeoutSeconds: 540, memory: '4GB' })
   .https.onRequest(async (request, response) => {
+    try {
+      if (request.method !== 'POST') {
+        console.error('not post method')
+        return response.status(400)
+      }
+
+      const token = request.headers.authorization?.split('Bearer ')[1]
+      if (!token) {
+        console.error('no token found')
+        return response.status(401)
+      }
+
+      const uid = await verify(token)
+
+      if (!uid) {
+        console.err('no uid found')
+        return response.status(401)
+      }
+    } catch (error) {
+      console.error(error)
+      return response.status(500)
+    }
+
     const bigQuery = new BigQuery({ projectId: 'tad-story' })
     const { openConnection, closeConnection, crwal } = useCrawling()
     const { browser, page } = await openConnection()
@@ -25,7 +50,7 @@ exports.crwalOneplusDeal = functions
         ],
       }
 
-      const result = await bigQuery
+      await bigQuery
         .dataset(process.env.DATA_SET)
         .table(process.env.TABLE)
         .insert(data, { schema })
@@ -33,10 +58,20 @@ exports.crwalOneplusDeal = functions
           console.error('ERROR:', err)
         })
 
-      response.status(201).send(result)
+      response.status(201)
     } catch (error) {
-      response.status(500).send(error)
+      response.status(500)
     } finally {
       await closeConnection(page, browser)
     }
   })
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  })
+  const payload = ticket.getPayload()
+  const userid = payload['sub']
+  return userid
+}
